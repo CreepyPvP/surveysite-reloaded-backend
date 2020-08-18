e080b21baa9650523e0f9f2e9e7cf77ae7fe992a
CreepyPvP <lufox2411@gmail.com>
2020-08-18 20:06:36 +0200

    first commit; basic spring boot setup

---
+++ src/main/java/fullbuild/de/surveysitereloaded/SurveysiteReloadedApplication.java
@@ -0,0 +1,13 @@
+package fullbuild.de.surveysitereloaded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveysiteReloadedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveysiteReloadedApplication.class, args);
+	}
+
+}
